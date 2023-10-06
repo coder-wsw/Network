6934e438d5b7c1d8955ea9d48a05ec74d7c2065b
coder-wsw <1045518769@qq.com>
2023-10-06 23:05:39 +0800

    修改

--- examples/server02/main.cpp
+++ examples/server02/main.cpp
@@ -42,49 +42,48 @@ int main()
                 ftell(htmlFile); // 获取当前文件指针位置，即文件长度
             rewind(htmlFile); // 将文件指针移到文件开头
             SPDLOG_INFO("file_size: {}", file_size);
-            char responseBody[5096];
+            char responseBody[2048];
             size_t total_in = 0;
             auto readSize = fread(responseBody, 1, file_size, htmlFile);
             rewind(htmlFile);
             SPDLOG_INFO("readSize: {}", readSize);
             char response[2048];
-            sprintf(response,
-                    "HTTP/1.1 200 OK\r\n"
-                    "Content-Length: %ld\r\n\r\n",
-                    4892);
-            send(clientSocket, response, strlen(response), 0);
-            while (total_in < file_size) {
-                char tmp[1024];
-                auto tmpSize = fread(tmp, 1, 100, htmlFile);
-                SPDLOG_INFO("tmpSize: {}", tmpSize);
-                SPDLOG_INFO("strlen(tmp): {}", strlen(tmp));
-                send(clientSocket, response, strlen(response), 0);
-                send(clientSocket, tmp, tmpSize, 0);
-                total_in += tmpSize;
-            }
+            // sprintf(response,
+            //         "HTTP/1.1 200 OK\r\n"
+            //         "Content-Length: %ld\r\n\r\n",
+            //         4892);
+            // send(clientSocket, response, strlen(response), 0);
+            // while (total_in < file_size) {
+            //     char tmp[1024];
+            //     auto tmpSize = fread(tmp, 1, 100, htmlFile);
+            //     SPDLOG_INFO("tmpSize: {}", tmpSize);
+            //     SPDLOG_INFO("strlen(tmp): {}", strlen(tmp));
+            //     send(clientSocket, tmp, tmpSize, 0);
+            //     total_in += tmpSize;
+            // }
 
             char compressed[1024];
 
-            // z_stream stream;
-            // stream.zalloc = Z_NULL;
-            // stream.zfree = Z_NULL;
-            // stream.opaque = Z_NULL;
-            // deflateInit2(&stream, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 31, 8,
-            //              Z_DEFAULT_STRATEGY);
-            // stream.next_in = (Bytef*)responseBody;
-            // stream.avail_in = strlen(responseBody);
-            // stream.next_out = (Bytef*)compressed;
-            // stream.avail_out = 2048;
-            // deflate(&stream, Z_FINISH);
-            // deflateEnd(&stream);
+            z_stream stream;
+            stream.zalloc = Z_NULL;
+            stream.zfree = Z_NULL;
+            stream.opaque = Z_NULL;
+            deflateInit2(&stream, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 31, 8,
+                         Z_DEFAULT_STRATEGY);
+            stream.next_in = (Bytef*)responseBody;
+            stream.avail_in = strlen(responseBody);
+            stream.next_out = (Bytef*)compressed;
+            stream.avail_out = 2048;
+            deflate(&stream, Z_FINISH);
+            deflateEnd(&stream);
 
-            // sprintf(response,
-            //         "HTTP/1.1 200 OK\r\n"
-            //         "Content-Encoding: gzip\r\n"
-            //         "Content-Length: %ld\r\n\r\n",
-            //         stream.total_out);
-            // send(clientSocket, response, strlen(response), 0);
-            // send(clientSocket, compressed, stream.total_out, 0);
+            sprintf(response,
+                    "HTTP/1.1 200 OK\r\n"
+                    "Content-Encoding: gzip\r\n"
+                    "Content-Length: %ld\r\n\r\n",
+                    stream.total_out);
+            send(clientSocket, response, strlen(response), 0);
+            send(clientSocket, compressed, stream.total_out, 0);
         }
         else {
             const char* response = "HTTP/1.1 200 OK\r\n"
